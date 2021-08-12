31fde8418e0b447ead0e1505425a87e98d7a8f21
rgcuffee <rgcuffee@gmail.com>
2021-08-12 11:11:10 -0700

    set up eslint / connected db

--- database/index.js
+++ database/index.js
@@ -2,16 +2,51 @@ const { Pool, Client } = require('pg');
 
 const { password } = require('../config/config.js')
 
-const connection = new Pool({
+const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'products',
-    password,
     port: 5432,
+    database: 'products',
+    password:'password'
 });
 
-connection.connect(() => {
-    console.log('🐘 🐘 DB CONNECTED 🐘 🐘')
+const client = new Client ({
+    user: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    database: 'products',
+    password: 'password'
 });
 
-module.exports = { connection }
+
+client.connect()
+.then(() => console.log('🐘 🐘 DB CONNECTED 🐘 🐘'))
+.catch(e => console.log)
+.finally(() => client.end())
+
+// async function poolInstance() {
+//     const pool = new Pool(credientials);
+//     const now = await pool.query('SELECT * FROM product_info WHERE product_id = 1;');
+//     await pool.end();
+
+//     return now;
+// }
+
+// async function clientInstance() {
+//     const client = new Client(credentials);
+//     await client.connect();
+//     const now = await client.query('SELECT * FROM product_info WHERE product_id = 1;');
+//     await client.end();
+  
+//     return now;
+//   }
+
+//   (async () => {
+//     const poolResult = await poolInstance();
+//     console.log(poolResult);
+  
+//     // const clientResult = await clientInstance();
+//     // console.log("Time with client: " + clientResult);
+//   })();
+
+module.exports = pool; 
--- server/index.js
+++ server/index.js
@@ -5,3 +5,17 @@ const db = require('../database/index')
 app.listen(3000, () => {
     console.log(`👂 👀 Listening on PORT 3000 👂 👀`);
 });
+
+
+
+
+
+const query = 'SELECT * FROM styles WHERE product_id = 1;'
+db.query(query, (err, result) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(result);
+        db.end();
+    }
+});

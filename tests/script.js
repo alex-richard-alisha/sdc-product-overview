bccc9e5071667a1f0a3b246e77ce80b58a244fa6
rgcuffee <rgcuffee@gmail.com>
2021-08-17 07:46:09 -0700

    stress test sweet spot

--- tests/script.js
+++ tests/script.js
@@ -11,14 +11,14 @@ export const requests = new Counter('http_reqs');
 
 export const options = {
   stages: [
-    { duration: '1m', target: 50 }, // below normal load
-    { duration: '2m', target: 50 },
-    { duration: '1m', target: 100 }, // normal load
-    { duration: '2m', target: 100 },
-    { duration: '1m', target: 150 }, // around the breaking point
-    { duration: '2m', target: 150 },
-    { duration: '1m', target: 200 }, // beyond the breaking point
-    { duration: '2m', target: 200 },
+    { duration: '1m', target: 1000 }, // below normal load
+    { duration: '2m', target: 1000 },
+    { duration: '1m', target: 2000 }, // normal load
+    { duration: '2m', target: 2000 }, //6m
+    { duration: '1m', target: 3000 }, // around the breaking point
+    { duration: '2m', target: 3000 }, //9m
+    { duration: '1m', target: 4000 }, // beyond the breaking point
+    { duration: '2m', target: 4000 },
     { duration: '5m', target: 0 }, // scale down. Recovery stage.
   ],
 };
@@ -26,6 +26,9 @@ export const options = {
 export default function () {
   const BASE_URL = 'http://localhost:3000/api';
 
+  // const checkRes = check(res, {
+  //   'status is 200': (r) => r.status === 200,
+  // });
   const res = http.batch([
     [
       'GET', `${BASE_URL}/products/1`,
@@ -39,10 +42,6 @@ export default function () {
   ]);
 
   sleep(1);
-
-  const checkRes = check(res, {
-    'status is 200': (r) => r.status === 200,
-  });
 }
 
-//k6 run tests/script.js
+// k6 run tests/script.js

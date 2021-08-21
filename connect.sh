d395be21a7c3c5b8ae2d0c1d51bb4bdc2c3c7db0
rgcuffee <rgcuffee@gmail.com>
2021-08-21 07:42:24 -0700

    ip update

--- connect.sh
+++ connect.sh
@@ -1,12 +1,26 @@
 case $1 in
 
 postgres)
-    scp -i hr.pem ./init.sh ec2-user@ec2-3-143-147-30.us-east-2.compute.amazonaws.com:~/init.sh
+    # scp -i hr.pem ./init.sh ec2-user@ec2-3-143-147-30.us-east-2.compute.amazonaws.com:~/init.sh
     ssh -i hr.pem ec2-user@ec2-3-143-147-30.us-east-2.compute.amazonaws.com
     ;;
 
-node)
-    scp -i hr.pem ./init.sh ec2-user@ec2-3-142-153-84.us-east-2.compute.amazonaws.com:~/init.sh
+node1)
+    # scp -i hr.pem ./init.sh ec2-user@ec2-3-142-153-84.us-east-2.compute.amazonaws.com:~/init.sh
     ssh -i hr.pem ec2-user@ec2-3-142-153-84.us-east-2.compute.amazonaws.com
+    ;;
+node2)
+    ssh -i hr.pem ec2-user@18.119.11.91
+    ;;
+node3)
+    ssh -i hr.pem ec2-user@18.222.237.21
+    ;;
+
+node4)
+    ssh -i hr.pem ec2-user@18.219.229.20
+    ;;
+
+nginx)
+    ssh -i hr.pem ec2-user@18.221.125.221
     ;;
 esac

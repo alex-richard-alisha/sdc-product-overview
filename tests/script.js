a544931c059d7d04b484ddf2b81064d5caed16ab
Richard Cuffee <rgcuffee@gmail.com>
2021-09-19 18:31:29 -0700

    k6

---
+++ tests/script.js
@@ -0,0 +1,18 @@
+import http from 'k6/http';
+
+export let options = {
+  discardResponseBodies: true,
+  scenarios: {
+    contacts: {
+      executor: 'constant-arrival-rate',
+      rate: 200, // 200 RPS, since timeUnit is the default 1s
+      duration: '1m',
+      preAllocatedVUs: 50,
+      maxVUs: 100,
+    },
+  },
+};
+
+export default function () {
+  http.get('http://localhost:3000/api/products/1/styles');
+}
